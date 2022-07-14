767d24438a0050a9afa8b9bdb55ebe35e76bd516
gwionrob <robertsongwion21@gmail.com>
2022-07-14 12:56:09 +0100

    replaced BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./pages/App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
 );
