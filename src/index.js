ffdcdbb9f37a58df0c75c78c704dbebeec7348d8
gwionrob <robertsongwion21@gmail.com>
2022-07-14 14:17:44 +0100

    added BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/img-compression-visualiser">
         <App />
     </BrowserRouter>,
 );
